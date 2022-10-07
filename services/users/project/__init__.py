f0012e5a1ab5c8160c03f52f966a35ac7d8866cf
Nikita Dushkov <gespi@bk.com>
2022-10-07 17:01:34 +0300

    add migrations and refactoring code

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,9 +1,14 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate
+
 
 # instantiate the db
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
+migrate = Migrate()
 
 
 def create_app(script_info=None):
@@ -16,6 +21,8 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprints
     from project.api.users import users_blueprint
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -5,12 +5,15 @@ class BaseConfig:
     """Base configuration"""
     TESTING = False
     SECRET_KEY = 'my_precious'
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(BaseConfig):
